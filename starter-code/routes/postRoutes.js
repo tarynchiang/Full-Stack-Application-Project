97997e8a51aaa88c93f40f33eb7859d9c4f19a7d
yi-ting Chiang <tarynchiang@gmail.com>
2019-07-13 18:24:19 -0400

    done with see the posts

--- starter-code/routes/postRoutes.js
+++ starter-code/routes/postRoutes.js
@@ -4,8 +4,11 @@ const Post    = require('../models/Post');
 
 const passport = require('passport');
 
+const uploadPhoto = require('../config/cloundinary-setup');
 
-router.get('/list-all-posts',(req,res,next)=>{
+
+//see the posts
+router.get('/posts',(req,res,next)=>{
     
     Post.find().populate('author')
     .then((allThePosts)=>{
@@ -23,10 +26,78 @@ router.get('/list-all-posts',(req,res,next)=>{
     .catch((err)=>{
         next(err);
     })
-
-
 });
 
+router.get('/posts/:country',(req,res,next)=>{
+
+    Post.find({country: req.params.country}).populate('author')
+    .then((allThePosts)=>{
+        
+        if(req.user){
+
+            allThePosts.forEach((eachPost)=>{
+                if(eachPost.author._id.equals(req.user._id)){
+                    eachPost.owned = true;
+                }
+            })
+        }
+        res.render('postViews/postsWithCountry',{posts: allThePosts})
+    })
+    .catch((err)=>{
+        next(err);
+    })
+});
+
+router.get('/posts/:country/:city',(req,res,next)=>{
+
+    Post.find({country: req.params.country, city:req.params.city}).populate('author')
+    .then((allThePosts)=>{
+        
+        if(req.user){
+
+            allThePosts.forEach((eachPost)=>{
+                if(eachPost.author._id.equals(req.user._id)){
+                    eachPost.owned = true;
+                }
+            })
+        }
+        res.render('postViews/postsWithCity',{posts: allThePosts})
+    })
+    .catch((err)=>{
+        next(err);
+    })
+});
+
+//create new post
+router.get('/posts/create-new/:country/:city',(req,res,next)=>{
+    
+    res.render('postViews/newPost');
+})
+
+router.post('/posts/create-new/:country/:city',uploadPhoto.single('thePic'),(res,req,next)=>{
+
+
+    let title = req.body.title;
+    let content = req.body.content;
+    let author = req.user._id;
+    let country = req.params.country;
+    //let region = ??
+    //let likes = ??
+    let city = req.params.city;
+    let img = req.file.url;
+    // let comments = ??
+
+    
+    // title: String,
+    // content: String,
+    // author: {type: Schema.Types.ObjectId, ref: 'User'},
+    // country:String,
+    // region:String,
+    // city:String,
+    // likes:[{ type : Schema.Types.ObjectId, ref: 'User' }],
+    // img: String,
+    // comments:[{ type : Schema.Types.ObjectId, ref: 'Comment' }]
+})
 
 
 
